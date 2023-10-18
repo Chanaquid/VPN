e103606ac53945de1af1eaac2115125607629e26
Joshi Chanakya <charuchanu126@gmail.com>
2023-10-18 14:16:11 +0200

    vpn_first commit

---
+++ Modern VPN/MVVM/ViewModel/ProtectionViewModel.cs
@@ -0,0 +1,100 @@
+﻿using Modern_VPN.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Modern_VPN.MVVM.ViewModel
+{
+    internal class ProtectionViewModel : ObservableObject
+    {
+        public ObservableCollection<ServerModel> Servers{ get; set; }
+        private string  _connectionStatus;
+
+        public string ConnectionStatus
+        {
+            get { return _connectionStatus; }
+            set 
+            { 
+                _connectionStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand ConnectCommand  { get; set; }
+        public ProtectionViewModel()
+        {
+            Servers = new ObservableCollection<ServerModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                Servers.Add(new ServerModel
+                {
+                    Country = "USA"
+                });
+                
+            }
+
+            ConnectCommand = new RelayCommand(o => 
+            {
+                Task.Run(() =>
+                {
+                    ConnectionStatus = "Connecting...";
+                    var process = new Process();
+                    process.StartInfo.FileName = "cmd.exe";
+                    process.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+                    process.StartInfo.ArgumentList.Add(@"/c rasdial MyServer vpnbook w8m9tb7 /phonebook: ./VPN/VPN.pbk");
+                    process.StartInfo.UseShellExecute = false;
+                    //process.StartInfo.CreateNoWindow = true;
+
+                    process.Start();
+                    process.WaitForExit();
+
+                    switch (process.ExitCode)
+                    {
+                        case 0:
+                            Debug.WriteLine("Success!");
+                            ConnectionStatus = "Connected!";
+                            break;
+                        case 691:
+                            Debug.WriteLine("Wrong credentials");
+                            ConnectionStatus = "Connection Failed! Retry";
+                            break;
+                        default:
+                            Debug.WriteLine($"Error: {process.ExitCode}");
+                            break;
+                    }
+                });
+
+            });
+        }
+        private void ServerBuilder()
+        {
+            var address = "US1.vpnbook.com";
+            var FolderPath = $"{Directory.GetCurrentDirectory()}/VPN";
+            var pbkPath = $"{FolderPath}/{address}.pbk";
+
+            if (!Directory.Exists(FolderPath))
+                Directory.CreateDirectory(FolderPath);
+
+            if (File.Exists(pbkPath))
+            {
+                MessageBox.Show("Connection already exists!");
+            }
+
+            var sb = new StringBuilder();
+            sb.AppendLine("[MyServer]");
+            sb.AppendLine("MEDIA=rastapi");
+            sb.AppendLine("Port=VPN2-0");
+            sb.AppendLine("Device=WAN miniport (IKEv2)");
+            sb.AppendLine("DEVICE=vpn");
+            sb.AppendLine("PhoneNumber={address}");
+            File.WriteAllText(pbkPath, sb.ToString());
+
+        }
+    }
+}
---
+++ Modern VPN/MVVM/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modern_VPN.MVVM.ViewModel
+{
+    internal class SettingsViewModel
+    {
+        public SettingsViewModel()
+        {
+
+        }
+    }
+}
